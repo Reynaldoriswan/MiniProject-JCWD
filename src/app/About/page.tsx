58cc48d4f750b62cab01a600c4842dcff2f6cdee
Reynaldo Riswan <rraldo89@gmail.com>
2024-07-07 21:46:48 +0700

    update dihalaman about

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 import styles from './about.module.css'
 
 export default function About() {
     return (
         <>
-        <h1 className='font-semibold text-center mt-10 text-3xl'>About EventManagement</h1>
+        <h1 className='font-semibold text-center my-10 text-3xl'>About EventManagement</h1>
         <div className={styles.about}>
         <div className={styles.aboutSect}>
             <p className='text-justify'>Tim kami akan merancang acara sesuai dengan kebutuhan anda dan membuka
@@ -18,25 +19,66 @@ export default function About() {
                 memberikan acara meriah untuk berbagai kalangan seperti anak-anak/sekolah dan-lain-lain
             </p>
         </div>
-         <img className={styles.eventImg} src='/images/Event.jpg' />
+         <img className={styles.eventImg} src='/images/Event.jpg' alt='event-image' />
        </div>
 
-                <div className='border- shadow w-72 h-96'>
-                    <div className='relative max-w-max'>
-                        <img src='/images/Sunset.jpg' alt='' />
-                        <p className='px-2 bg-gray-500 rounded-sm absolute bottom-3 left-3
-                        bg-opacity-80 text-white'>$20 to come</p>
-                        <img className='w-14 h-14 absolute -bottom-7 right-2 border rounded-full border-gray-400' 
-                        src='/images/profile1.jpg' alt='' />
-                    </div>  
-                    
-                    <div className='p-3'>
-                        <p>Sunset in California</p>
-                        <p className='text-gray-400 text-sm mt-3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas architecto magnam ab amet 
-                            aut nostrum, veniam facere? Esse molestiae amet voluptates? Dolor exercitationem dolores quaerat consequuntur 
-                            et aliquid  eveniet!</p>
+            <div className='h-auto w-fit mx-auto flex flex-wrap items-center 
+            justify-center px-4 sm:px-0 pt-5 sm:pt-0 relative p-10 rounded-lg justify-items-center'>
+                   
+                    <div className='border shadow w-full lg:my-10 sm:w-72 h-96 pb-3'>
+                        <div className='card-secondary absolute flex items-center 
+                        justify-center inset-0 text-white font-bold'></div>
+
+                        <div className='items-card relative sm:max-w-max w-full'>
+                            <img className='w-full' src='/images/Sunset.jpg' alt='' />
+                            <p className='px-2 bg-gray-500 rounded-sm absolute bottom-3 left-3
+                            bg-opacity-80 text-white'>$20 to come</p>
+                            <img className='w-14 h-14 absolute -bottom-7 right-2 border rounded-full border-gray-400' 
+                            src='/images/profile1.jpg' alt='profile' />
+                        </div>  
+                        
+                        <div className='items-card p-3 bg-white border-gray-500'>
+                            <p>Sunset in California</p>
+                            <p className='text-black text-sm mt-3 text-justify'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas architecto magnam ab amet 
+                                aut nostrum, veniam facere? Esse molestiae amet voluptates? Dolor exercitationem dolores quaerat consequuntur 
+                                et aliquid  eveniet!</p>
+                        </div>
                     </div>
+
+                    <div className='card border shadow w-full lg:my-10 sm:w-72 h-96 pb-3 ml-0 sm:ml-5 mt-5 sm:mt-0'>
+                        <div className='items-card relative sm:max-w-max w-full'>
+                            <img className='w-full' src='/images/Sunset.jpg' alt='' />
+                            <p className='px-2 bg-gray-500 rounded-sm absolute bottom-3 left-3
+                            bg-opacity-80 text-white'>$20 to come</p>
+                            <img className='w-14 h-14 absolute -bottom-7 right-2 border rounded-full border-gray-400' 
+                            src='/images/profile1.jpg' alt='profile' />
+                        </div>  
+                    
+                        <div className='items-card p-3 bg-white border-gray-500'>
+                            <p>Sunset in California</p>
+                            <p className='text-black text-sm mt-3 text-justify'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas architecto magnam ab amet 
+                                aut nostrum, veniam facere? Esse molestiae amet voluptates? Dolor exercitationem dolores quaerat consequuntur 
+                                et aliquid  eveniet!</p>
+                        </div>
                 </div>
+
+                <div className='card border shadow w-full lg:my-10 sm:w-72 h-96 pb-3 ml-0 sm:ml-5 mt-5 sm:mt-0'>
+                        <div className='items-card relative sm:max-w-max w-full'>
+                            <img className='w-full' src='/images/Sunset.jpg' alt='' />
+                            <p className='px-2 bg-gray-500 rounded-sm absolute bottom-3 left-3
+                            bg-opacity-80 text-white'>$20 to come</p>
+                            <img className='w-14 h-14 absolute -bottom-7 right-2 border rounded-full border-gray-400' 
+                            src='/images/profile1.jpg' alt='profile' />
+                        </div>  
+                    
+                        <div className='items-card p-3 bg-white border-black'>
+                            <p>Sunset in California</p>
+                            <p className='text-black text-sm mt-3 text-justify'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas architecto magnam ab amet 
+                                aut nostrum, veniam facere? Esse molestiae amet voluptates? Dolor exercitationem dolores quaerat consequuntur 
+                                et aliquid  eveniet!</p>
+                        </div>
+                </div>
+            </div>
        </>
     );
 }
