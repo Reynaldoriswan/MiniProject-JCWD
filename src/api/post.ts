f99c972ad98f3b80d0d9a3d35d2f18e37b00830e
GDoubleSix <muqigewa84@gmail.com>
2024-07-19 09:47:56 +0700

    D1: Try this shit, create  auth.ts registerProcess

--- src/api/post.ts
+++ src/api/post.ts
@@ -1,29 +1,37 @@
-"use server"
-import axios from "axios"
-import * as jose from "jose"
-import { redirect } from 'next/navigation'
-import { cookies } from "next/headers"
-const base_api = "http://localhost:5670"
+"use server";
+import axios from "axios";
+import * as jose from "jose";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
-const authToken = cookies().get("authToken")?.value as any
+const base_api = "http://localhost:5670";
+
+const authToken = cookies().get("authToken")?.value as any;
 
 export async function getAllPosts() {
-    const res = await axios.get(base_api + "/post")
-    return res
+  const res = await axios.get(base_api + "/post");
+  return res;
 }
 
 export async function createPost(params: any) {
-    const session = await jose.jwtVerify(authToken, new TextEncoder().encode("mySecretAcademia"))
-    console.log("session => ", session)
-    const res = await axios.post(base_api + "/post", {
-        title: params.title,
-        body: params.body,
-        user_id: session.payload.id
-    }, {
-        headers: {
-            Authorization: "Bearer " + authToken
-        }
-    })
+  const session = await jose.jwtVerify(
+    authToken,
+    new TextEncoder().encode("mySecretAcademia")
+  );
+  console.log("session => ", session);
+  const res = await axios.post(
+    base_api + "/post",
+    {
+      title: params.title,
+      body: params.body,
+      user_id: session.payload.id,
+    },
+    {
+      headers: {
+        Authorization: "Bearer " + authToken,
+      },
+    }
+  );
 
-    redirect("/post")
+  redirect("/post");
 }
